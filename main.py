4fe0e6e4f8df315133bb8ebd426a4a43934e6e65
attikoszto <131347235+attikoszto@users.noreply.github.com>
2024-09-26 18:11:53 +0200

    add counter add report function

--- main.py
+++ main.py
@@ -9,7 +9,28 @@ def get_book_text(book_path):
 def main():
     book_path = 'books/frankenstein.txt'
     text = get_book_text(book_path)
-    print(text)
+    words = text.split()
+    lower_text = text.lower()
+    text_report(count_words = len(words),letters = count_each_letter(lower_text))
     
     
+
+def count_each_letter(text):
+    letter_count = {}
+    for letter in text:
+        if letter.isalpha():
+            if letter in letter_count:
+                letter_count[letter] += 1
+            else:
+                letter_count[letter] = 1
+    return letter_count
+
+
+def text_report(count_words,letters):  
+    print(f"{count_words} words was found in the text")
+    
+    for key,value in letters.items() :
+        print(f"we found in the text the letter {key} {value} times")
+    print("end of the report")
+        
 main()
